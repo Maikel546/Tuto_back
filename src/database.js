a3659787669ff0ad6004fed100facd614486c083
Maikel Cardenas <maikel546@gmail.com>
2020-05-06 20:36:47 -0400

    first commit

---
+++ src/database.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+mongoose.connect ("mongodb://localhost/graphqlreactdb",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(db => console.log('Database is connected'))
+    .catch(err => console.log(err));
